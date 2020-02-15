9490ce3e64e907347d6dbf6322c46d15cbca5611
Michal Suchodolski <alfaomega2804@gmail.com>
2020-02-15 12:40:13 +0100

    NrKsiazkiValidatorTest

--- src/test/java/NrKsiazkiValidatorTest.java
+++ src/test/java/NrKsiazkiValidatorTest.java
@@ -20,6 +20,74 @@ public class NrKsiazkiValidatorTest {
         Assert.assertFalse(result);
     }
 
+    @Test
+    public void shouldCheckEmptyNrKsiazki(){
+        //given
+        String NrKsiazki = "";
+
+
+        //when
+
+        boolean result = NrKsiazkiValidator.valid(NrKsiazki);
+
+
+        //then
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void shouldCheckIf6Chars() {
+        //given
+        String NrKsiazki = "123456789";
+
+        //when
+        boolean result = NrKsiazkiValidator.valid(NrKsiazki);
+
+        //then
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void shouldCheckIfTooManyChars() {
+        //given
+        String NrKsiazki = "123456896874";
+
+        //when
+        boolean result = NrKsiazkiValidator.valid(NrKsiazki);
+
+        //then
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void shouldCheckIfNumbers() {
+        //given
+        String NrKsiazki = "123as4";
+
+        //when
+        boolean result = NrKsiazkiValidator.valid(NrKsiazki);
+        //then
+
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void shuldCheckIf6Nums() {
+        //given
+
+        String NrKsiazki = "123456";
+
+        //when
+        boolean result = NrKsiazkiValidator.valid(NrKsiazki);
+
+        //then
+
+        Assert.assertFalse(result);
+    }
+
+
+
+
 
 
 
